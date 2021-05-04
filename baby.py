b0ebaa9de487e3f6ae307baa577feab3d344b483
m214368 <73185501+m214368@users.noreply.github.com>
2021-05-04 00:22:26 -0400

    fixed spacing

--- baby.py
+++ baby.py
@@ -9,88 +9,88 @@ def dist(n1,n2):
     return math.sqrt((n1[0]-n2[0])**2 +(n1[1]-n2[1])**2)
 
 class Node:
-  def __init__(self, start, goals, robot):
-	self.locations = start
-	self.goals = goals
-	self.parent = None
-	self.action = None
-	self.weight = 7
-	self.robot = robot
+    def __init__(self, start, goals, robot):
+        self.locations = start
+        self.goals = goals
+        self.parent = None
+        self.action = None
+        self.weight = 7
+        self.robot = robot
 
-  def drive(self, place, locs):
-	node = Node(deepcopy(self.locations), deepcopy(self.goals), deepcopy(self.robot))
-	node.action = "drive " + str(locs[place])
-	node.parent = self
-	node.robot.loc = locs[place]
-	#update balloon locations
-	for i in self.robot.carrying:
-		node.locations[i] = locs[place]
-	node.weight = 7 
-	return node
+    def drive(self, place, locs):
+        node = Node(deepcopy(self.locations), deepcopy(self.goals), deepcopy(self.robot))
+        node.action = "drive " + str(locs[place])
+        node.parent = self
+        node.robot.loc = locs[place]
+        #update balloon locations
+        for i in self.robot.carrying:
+            node.locations[i] = locs[place]
+        node.weight = dist(self.robot.loc, locs[place])
+        return node
 
-  def pickup(self, color):
-	node = Node(deepcopy(self.locations), deepcopy(self.goals), deepcopy(self.robot))
-	node.action = "pickup " + color
-	node.robot.pickup(color)
-	node.weight = 3
-	node.parent = self
-	return node
+    def pickup(self, color):
+        node = Node(deepcopy(self.locations), deepcopy(self.goals), deepcopy(self.robot))
+        node.action = "pickup " + color
+        node.robot.pickup(color)
+        node.weight = 3
+        node.parent = self
+        return node
 
-  def putdown(self, color):
-	node = Node(deepcopy(self.locations), deepcopy(self.goals), deepcopy(self.robot))
-	node.action = "putdown " + color
-	node.robot.putdown(color)
-	node.locations[color] = node.robot.loc
-	node.weight = 3
-	node.parent = self
-	return node
+    def putdown(self, color):
+        node = Node(deepcopy(self.locations), deepcopy(self.goals), deepcopy(self.robot))
+        node.action = "putdown " + color
+        node.robot.putdown(color)
+        node.locations[color] = node.robot.loc
+        node.weight = 3
+        node.parent = self
+        return node
 
-  def heuristic(self):
-    h = 0
-    for key,value in self.goals.items():
-      h+=dist(value,self.locations[key])
-    h = h/2
-    return h
-	
-  def generate_children(self):
-	childs = list()
-	if len(self.robot.carrying) < 2:
-	  for i in self.locations:
-		temp = self.drive(i, self.locations)
-		childs.append(temp)
-	  for i in self.locations:
-		if self.locations[i] == self.robot.loc and i not in self.robot.carrying:
-			temp = self.pickup(i)
-			childs.append(temp)
-	else:
-	  for i in self.robot.carrying:
-		if self.robot.loc is not self.goals[i]:
-			temp = self.drive(i, self.goals)
-			childs.append(temp)
-	  for i in self.robot.carrying:
-		if self.goals[i] == self.robot.loc:
-		        print i
-			temp = self.putdown(i)
-			childs.append(temp)
-	return childs
+    def heuristic(self):
+        h = 0
+        for key,value in self.goals.items():
+            h+=dist(value,self.locations[key])
+        h = h/2
+        return h
+    
+    def generate_children(self):
+        childs = list()
+        if len(self.robot.carrying) < 2:
+            for i in self.locations:
+                temp = self.drive(i, self.locations)
+                childs.append(temp)
+            for i in self.locations:
+                if self.locations[i] == self.robot.loc and i not in self.robot.carrying:
+                    temp = self.pickup(i)
+                    childs.append(temp)
+        else:
+            for i in self.robot.carrying:
+                if self.robot.loc is not self.goals[i]:
+                    temp = self.drive(i, self.goals)
+                    childs.append(temp)
+            for i in self.robot.carrying:
+                if self.goals[i] == self.robot.loc:
+                    print i
+                    temp = self.putdown(i)
+                    childs.append(temp)
+        return childs
 
-  def State(self):
+    def State(self):
         self.state = ""
         for key,value in sorted(self.locations.items()):
             self.state += str(key)+":"+str(value)
         for value in sorted(self.robot.carrying):
             self.state += "carrying:"+str(value)
         return self.state
-  def RState(self):
+    def RState(self):
         self.state = ""
         for key,value in sorted(self.locations.items()):
             self.state += str(key)+":"+str(value)
         for value in sorted(self.robot.carrying):
             self.state += "carrying:"+str(value)
-	self.state += " ROBOT: " + str(self.robot.loc)
+        self.state += " ROBOT: " + str(self.robot.loc)
         return self.state
 
-  def goalState(self):
+    def goalState(self):
         self.state = ""
         for key,value in sorted(self.goals.items()):
             self.state += str(key)+":"+str(value)
@@ -98,48 +98,46 @@ class Node:
             self.state += "carrying:"+str(value)
         return self.state
 
-
-	 
-
 class Rob:
-  def __init__(self, start):
-	self.loc = start
-	self.carrying = []
-  def pickup(self, color):
-	self.carrying.append(color)
-  def putdown(self, color):
-	self.carrying.remove(color) 
+    def __init__(self, start):
+        self.loc = start
+        self.carrying = []
+    def pickup(self, color):
+        self.carrying.append(color)
+    def putdown(self, color):
+        self.carrying.remove(color) 
 
 
 class Searcher:
     def search(self):
         #start file
-	startFile = "start.json"
-	endFile = "simple.json"
-	with open(startFile) as json_file:
+        startFile = "start.json"
+        endFile = "simple.json"
+        with open(startFile) as json_file:
             startLoc = json.load(json_file)
         with open(endFile) as json_file:
             endLoc = json.load(json_file)
 
-	startRob = "0,0"
-	robot = Rob(startRob)
-	startN = Node(startLoc, endLoc, robot)
-	seen = dict()
-	cur = deepcopy(startN)
-	pq = PriQue()
-	while cur.State() != startN.goalState():
-	  print cur.RState()
-	  print "//////////"
-	  for next in cur.generate_children():
-	    #print next.action
-	    print next.RState()
-	    if next.State() not in seen:
-		pq.insert(next.weight+next.heuristic(), next)
-                seen[next.State()] = next.weight;
-	  cur = pq.getMin()
-	  print "*******"
+        startRob = (0,0)
+        robot = Rob(startRob)
+        startN = Node(startLoc, endLoc, robot)
+        seen = dict()
+        cur = deepcopy(startN)
+        pq = PriQue()
+        while cur.State() != startN.goalState():
+            print cur.RState()
+            print cur.heuristic()
+            print "//////////"
+            for next in cur.generate_children():
+            #print next.action
+               print next.RState()
+            if next.State() not in seen:
+                pq.insert(next.weight+next.heuristic(), next)
+                seen[next.State()] = next.weight
+            cur = pq.getMin()
+            print "*******"
 
-	path = []
+        path = []
         #get path
         path.insert(0, cur.action)
         while cur.parent is not None:
@@ -147,11 +145,9 @@ class Searcher:
             cur = cur.parent
         print path
         return path
-	
-	
+    
+    
 
 s = Searcher()
 s.search()
-	
-	
-	
+
